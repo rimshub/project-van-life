7ff41e4d7626c23ae6e7a298d706d805fcee975c
Rimsha-maker <55735255+Rimsha-maker@users.noreply.github.com>
2023-11-03 22:12:18 +0500

    fixed path error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Home from './pages/Home.jsx';
-import About from './pages/About.jsx';
-import Navbar from './components/Navbar.jsx';
+import Home from './pages/home.jsx';
+import About from './pages/about.jsx';
+import Navbar from './components/navbar.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './server.js';
